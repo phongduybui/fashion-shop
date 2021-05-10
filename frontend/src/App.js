511fc44d75888b2014e2b0371d4674b77e5f7246
phongduybui <buiduyphong921@gmail.com>
2021-05-10 21:56:24 +0700

    base layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
