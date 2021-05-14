3f78c32cfc59a173afba7198b91ea0c275066646
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-14 21:48:15 +0700

    add redux to project

---
+++ frontend/src/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers.js';
+
+const reducers = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
+
+const initialState = {};
+
+const middlewares = [thunk];
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+export default store;
