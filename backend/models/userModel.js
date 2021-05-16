26da299a2aeda116eba55add2e636e2cc967674a
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-16 16:43:29 +0700

    hash password and compare pwd

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,23 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Check password when login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password before save it to database
+userSchema.pre('save', async function (next) {
+  // Neu edit profile ma khong phai la password thi khong hash lai password
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  // Generate number of rounds
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
