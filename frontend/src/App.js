fb18c8cfda78edf6aad8ec3ad8fc9e0b4ed9aa0f
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-18 22:06:03 +0700

    ADD: PaymentScreen and ShippingScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const App = () => {
   return (
@@ -18,6 +22,10 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
         </Container>
       </main>
       <Footer />
