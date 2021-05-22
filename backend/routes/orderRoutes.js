5f2ae83d3e1fafe41da255e5b221fbb00ac8ddf8
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-22 23:26:30 +0700

    BACKEND: orderControllers and routes (getMyOrders, updateOrderToPaid)

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 import {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
 } from '../controllers/orderControllers.js';
 import protect from '../middlewares/authMiddleware.js';
 
+router.route('/myorders').get(protect, getMyOrders);
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
