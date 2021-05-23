17a0ed4b36c3aa23bb37754d60ec2a52919411a0
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-23 22:02:24 +0700

    BACKEND: Complete productControllers, productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,12 +2,22 @@ import express from 'express';
 import {
   getProducts,
   getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
 } from '../controllers/productController.js';
-
+import { protect, checkAdmin } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
-router.route('/').get(getProducts);
-
-router.route('/:id').get(getProductById);
+router.route('/').get(getProducts).post(protect, checkAdmin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
+router.get('/top', getTopProducts);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, checkAdmin, deleteProduct)
+  .put(protect, checkAdmin, updateProduct);
 
 export default router;
