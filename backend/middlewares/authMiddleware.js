888f4777b5d6e48788808f4f0499b21acccf01cc
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-23 22:04:39 +0700

    BACKEND: checkAdmin middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -30,4 +30,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-export default protect;
+const checkAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized, not admin');
+  }
+};
+
+export { checkAdmin, protect };
