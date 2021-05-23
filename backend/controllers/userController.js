5b52087e58395433b00eb057df8e174640e777a4
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-23 22:04:05 +0700

    BACKEND: Admin features - getUsers, deleteUser, getUserById, updateUser endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -113,4 +113,83 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+/**
+ * @desc    Get all users
+ * @route   GET /api/users
+ * @access  Private/Admin only
+ */
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+/**
+ * @desc    Get all users
+ * @route   GET /api/users
+ * @access  Private/Admin only
+ */
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({ message: 'User removed' });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+/**
+ * @desc    Get user by Id
+ * @route   GET /api/users/:id
+ * @access  Private/Admin only
+ */
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+/**
+ * @desc    Update user
+ * @route   PUT /api/users/:id
+ * @access  Private/Admin only
+ */
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    const { name, email, isAdmin } = req.body;
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.isAdmin = isAdmin;
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,16 +4,25 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
-import protect from '../middlewares/authMiddleware.js';
+import { protect, checkAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', registerUser);
+router.route('/').post(registerUser).get(protect, checkAdmin, getUsers);
 router.post('/login', authUser);
 router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+router
+  .route('/:id')
+  .delete(protect, checkAdmin, deleteUser)
+  .get(protect, checkAdmin, getUserById)
+  .put(protect, checkAdmin, updateUser);
 
 export default router;
