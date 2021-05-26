93a708e14276da929f00a5fb5a24c5263a9d0145
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-26 22:44:04 +0700

    Install morgan

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -17,6 +18,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.send('API is running');
