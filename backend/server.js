6e08266deb1c16839a388103359d5b4f9848ab4c
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-26 23:36:54 +0700

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 }
-
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -38,6 +37,14 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+}
 
 app.use(notFound);
 app.use(errorHandler);
