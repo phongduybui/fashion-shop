333cc8e25c7d0fbb750885429515c79ba89179fa
Phong Duy Bui <buiduyphong921@gmail.com>
2021-05-30 22:58:11 +0700

    Frontend: Install Sass, configure to convert library to Material UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store.js';
-import './bootstrap.min.css';
-import './index.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './themes/theme';
+// import './bootstrap.min.css';
+import './assets/scss/style.scss';
+
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
---
+++ frontend/src/themes/theme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['Jost', 'Roboto', 'sans-serif'].join(','),
+  },
+});
+
+export default theme;
