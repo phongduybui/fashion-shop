37305410cd73d4531a9a37ca67b5794b322db328
Phong Duy Bui <buiduyphong921@gmail.com>
2021-06-06 21:37:56 +0700

    Frontend: Complete HomeScreen UI

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,58 +1,89 @@
 import React, { useEffect } from 'react';
 import queryString from 'query-string';
 import { Link } from 'react-router-dom';
-import { Col, Row } from 'react-bootstrap';
-import Product from '../components/Product';
 import { useDispatch, useSelector } from 'react-redux';
-import { listProducts } from '../actions/productActions';
+import { listTopProducts } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import Paginate from '../components/Paginate';
 import Meta from '../components/Meta';
-import ProductCarousel from '../components/ProductCarousel';
+import HomeCarousel from '../components/Home/HomeCarousel';
+import Container from '@material-ui/core/Container';
+import HomeBanner from '../components/Home/HomeBanner';
+import ProductCard from '../components/Product/ProductCard';
+import { CircularProgress, Grid, Typography } from '@material-ui/core';
+import ProductTabs from '../components/Product/ProductTabs';
+import HomeService from '../components/Home/HomeService';
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+
+const useStyles = makeStyles((theme) => ({
+  loading: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    marginTop: 40,
+    marginBottom: 40,
+  },
+}));
 
 const HomeScreen = ({ location }) => {
-  const { keyword, pageNumber = 1 } = queryString.parse(location.search);
+  const classes = useStyles();
   const dispatch = useDispatch();
-  const productList = useSelector((state) => state.productList);
+  const { keyword, pageNumber = 1 } = queryString.parse(location.search);
+  const productTopRated = useSelector((state) => state.productTopRated);
 
-  const { loading, error, products, page, pages } = productList;
+  const {
+    loading: loadingProductTop,
+    errorProductTop,
+    products: productTop,
+  } = productTopRated;
 
   useEffect(() => {
-    dispatch(listProducts(keyword, pageNumber));
+    dispatch(listTopProducts());
   }, [dispatch, keyword, pageNumber]);
 
   return (
     <>
       <Meta />
       {!keyword ? (
-        <ProductCarousel />
+        <>
+          <HomeCarousel />
+        </>
       ) : (
         <Link to='/' className='btn btn-light'>
           Go Back
         </Link>
       )}
-      <h1 className='mt-5'>Latest Products</h1>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant='danger'>{error}</Message>
-      ) : (
-        <>
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-          <Paginate
-            pages={pages}
-            page={page}
-            keyword={keyword ? keyword : ''}
-          />
-        </>
-      )}
+      <Container maxWidth='xl'>
+        <Typography
+          variant='h4'
+          component='h2'
+          align='center'
+          style={{ margin: '60px 0 30px' }}
+        >
+          Top Products
+        </Typography>
+        {loadingProductTop ? (
+          <div className={classes.loading}>
+            <CircularProgress color='secondary' />
+          </div>
+        ) : errorProductTop ? (
+          <Alert severity='error'>{errorProductTop}</Alert>
+        ) : (
+          <>
+            <Grid container spacing={3} className={classes.topProducts}>
+              {productTop.map((product) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={product._id}>
+                  <ProductCard {...product} />
+                </Grid>
+              ))}
+            </Grid>
+            <HomeBanner />
+            <ProductTabs />
+          </>
+        )}
+      </Container>
+      <HomeService />
     </>
   );
 };
