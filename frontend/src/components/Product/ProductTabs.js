7feb0883ce0c0052d3f49af62ca312fa3e52ef1e
Phong Duy Bui <buiduyphong921@gmail.com>
2021-06-07 23:48:27 +0700

    Frontend: Dump data ProductTabs

--- frontend/src/components/Product/ProductTabs.js
+++ frontend/src/components/Product/ProductTabs.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  listProducts,
+  listLatestProducts,
+  listSaleProducts,
+} from '../../actions/productActions';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import ProductCard from './ProductCard';
-import { Grid } from '@material-ui/core';
+import { Button, CircularProgress, Grid } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -64,15 +70,57 @@ const useStyles = makeStyles((theme) => ({
       fontSize: '12px',
     },
   },
+  buttonMore: {
+    marginTop: 30,
+    ...theme.mixins.customize.centerFlex(),
+  },
+  loading: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    marginTop: 40,
+    marginBottom: 40,
+  },
 }));
 
-export default function SimpleTabs() {
+const ProductTabs = () => {
   const [value, setValue] = React.useState(0);
+  const dispatch = useDispatch();
   const classes = useStyles();
   const matches = useMediaQuery('(max-width:600px)');
 
+  const productLatest = useSelector((state) => state.productLatest);
+  const {
+    loading: loadingProductLatest,
+    error: errorProductLatest,
+    products: productsLatest,
+  } = productLatest;
+
+  const productSale = useSelector((state) => state.productSale);
+  const {
+    loading: loadingProductSale,
+    error: errorProductSale,
+    products: productsSale,
+  } = productSale;
+
+  const productList = useSelector((state) => state.productList);
+  const {
+    loading: loadingProductList,
+    error: errorProductList,
+    products: productsList,
+  } = productList;
+
+  useEffect(() => {
+    dispatch(listLatestProducts());
+  }, [dispatch]);
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    if (newValue === 1 && !productsSale.length) {
+      dispatch(listSaleProducts());
+    } else if (newValue === 2 && !productsList.length) {
+      dispatch(listProducts());
+    }
   };
 
   return (
@@ -97,42 +145,77 @@ export default function SimpleTabs() {
             label='Sale Products'
             {...a11yProps(1)}
           />
-          <Tab className={classes.tab} label='Best Rating' {...a11yProps(2)} />
+          <Tab className={classes.tab} label='All Products' {...a11yProps(2)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
         <Grid container spacing={4}>
-          {Array(8)
-            .fill()
-            .map((x, i) => (
-              <Grid item key={i + 1} xs={12} sm={4} md={3}>
-                <ProductCard />
+          {loadingProductLatest ? (
+            <div className={classes.loading}>
+              <CircularProgress color='secondary' />
+            </div>
+          ) : errorProductLatest ? (
+            <Alert severity='error'>{errorProductLatest}</Alert>
+          ) : (
+            productsLatest.map((product) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={product._id}>
+                <ProductCard {...product} />
               </Grid>
-            ))}
+            ))
+          )}
         </Grid>
+        <div className={classes.buttonMore}>
+          <Button variant='contained' color='secondary'>
+            Discover More
+          </Button>
+        </div>
       </TabPanel>
       <TabPanel value={value} index={1}>
         <Grid container spacing={4}>
-          {Array(8)
-            .fill()
-            .map((x, i) => (
-              <Grid item key={i + 1} xs={12} sm={4} md={3}>
-                <ProductCard />
+          {loadingProductSale ? (
+            <div className={classes.loading}>
+              <CircularProgress color='secondary' />
+            </div>
+          ) : errorProductSale ? (
+            <Alert severity='error'>{errorProductSale}</Alert>
+          ) : (
+            productsSale.map((product) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={product._id}>
+                <ProductCard {...product} />
               </Grid>
-            ))}
+            ))
+          )}
         </Grid>
+        <div className={classes.buttonMore}>
+          <Button variant='contained' color='secondary'>
+            Discover More
+          </Button>
+        </div>
       </TabPanel>
       <TabPanel value={value} index={2}>
         <Grid container spacing={4}>
-          {Array(8)
-            .fill()
-            .map((x, i) => (
-              <Grid item key={i + 1} xs={12} sm={4} md={3}>
-                <ProductCard />
+          {loadingProductList ? (
+            <div className={classes.loading}>
+              <CircularProgress color='secondary' />
+            </div>
+          ) : errorProductList ? (
+            <Alert severity='error'>{errorProductList}</Alert>
+          ) : (
+            productsList.map((product) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={product._id}>
+                <ProductCard {...product} />
               </Grid>
-            ))}
+            ))
+          )}
         </Grid>
+        <div className={classes.buttonMore}>
+          <Button variant='contained' color='secondary'>
+            Discover More
+          </Button>
+        </div>
       </TabPanel>
     </div>
   );
-}
+};
+
+export default ProductTabs;
