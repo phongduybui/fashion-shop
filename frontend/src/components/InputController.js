49bdef71e4d66600d3e2ff126c99f2defe8abf46
Phong Duy Bui <buiduyphong921@gmail.com>
2021-06-14 22:11:22 +0700

    Frontend: Add InputController component

---
+++ frontend/src/components/InputController.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { useFormContext, Controller } from 'react-hook-form';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& label': {
+      fontSize: 14,
+    },
+  },
+}));
+
+const InputController = ({
+  type,
+  name,
+  label,
+  required,
+  rules,
+  InputProps,
+}) => {
+  const classes = useStyles();
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue=''
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
+        <TextField
+          type={type}
+          label={label}
+          value={value}
+          onChange={onChange}
+          error={!!error}
+          helperText={error ? error.message : null}
+          InputProps={value ? InputProps : undefined}
+          className={classes.root}
+        />
+      )}
+      rules={{ required: required && `${label} is required`, ...rules }}
+    />
+  );
+};
+
+InputController.defaultProps = {
+  type: 'text',
+};
+
+export default InputController;
