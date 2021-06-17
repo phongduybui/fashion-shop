c720b35cdd141a9c2d4e27288c8c5a73b8d93af0
Phong Duy Bui <buiduyphong921@gmail.com>
2021-06-17 23:02:53 +0700

    Frontend: Update CheckoutSteps component  v2.0

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,51 +1,45 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
 
-const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  stepper: {
+    backgroundColor: theme.palette.background.default,
+    padding: 0,
+    '& .MuiStepIcon-completed, .MuiStepIcon-active': {
+      color: theme.palette.secondary.main,
+    },
+  },
+}));
+
+function getSteps() {
+  return [
+    'Sign In',
+    'Shipping Address',
+    'Choose Payment Method',
+    'Place Order',
+  ];
+}
+
+export default function CheckoutSteps({ step }) {
+  const classes = useStyles();
+  const steps = getSteps();
+
   return (
-    <Nav className='justify-content-center mb-4'>
-      <Nav.Item>
-        {step1 ? (
-          <LinkContainer to='/login'>
-            <Nav.Link>Sign In</Nav.Link>
-          </LinkContainer>
-        ) : (
-          <Nav.Link disabled>Sign In</Nav.Link>
-        )}
-      </Nav.Item>
-
-      <Nav.Item>
-        {step2 ? (
-          <LinkContainer to='/shipping'>
-            <Nav.Link>Shipping</Nav.Link>
-          </LinkContainer>
-        ) : (
-          <Nav.Link disabled>Shipping</Nav.Link>
-        )}
-      </Nav.Item>
-
-      <Nav.Item>
-        {step3 ? (
-          <LinkContainer to='/payment'>
-            <Nav.Link>Payment</Nav.Link>
-          </LinkContainer>
-        ) : (
-          <Nav.Link disabled>Payment</Nav.Link>
-        )}
-      </Nav.Item>
-
-      <Nav.Item>
-        {step4 ? (
-          <LinkContainer to='/placeorder'>
-            <Nav.Link>Place Order</Nav.Link>
-          </LinkContainer>
-        ) : (
-          <Nav.Link disabled>Place Order</Nav.Link>
-        )}
-      </Nav.Item>
-    </Nav>
+    <div className={classes.root}>
+      <Stepper activeStep={step} alternativeLabel className={classes.stepper}>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </div>
   );
-};
-
-export default CheckoutSteps;
+}
