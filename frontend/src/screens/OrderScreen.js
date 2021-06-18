92eadd647c0f0ca084f7dc76c709737e3717f91b
Phong Duy Bui <buiduyphong921@gmail.com>
2021-06-18 21:17:47 +0700

    Frontend: Update OrderScreen  v2.0

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,18 +1,109 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { PayPalButton } from 'react-paypal-button-v2';
-import { Link } from 'react-router-dom';
-import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import { PayPalButton } from 'react-paypal-button-v2';
+import { useDispatch, useSelector } from 'react-redux';
 import { getOrderById, payOrder, deliverOrder } from '../actions/orderActions';
+import { Link as RouterLink } from 'react-router-dom';
 import {
   ORDER_DELIVER_RESET,
   ORDER_PAY_RESET,
 } from '../constants/orderConstants';
+import {
+  Container,
+  Grid,
+  Paper,
+  Typography,
+  Breadcrumbs,
+  Link,
+  Divider,
+  ListItemText,
+  ListItem,
+  List,
+  ListItemIcon,
+  Avatar,
+  Box,
+  Hidden,
+  ListItemAvatar,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Button,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { GrLocation, GrCreditCard, GrProjects, GrUser } from 'react-icons/gr';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import Meta from '../components/Meta';
+import paypalImage from '../assets/images/paypal.png';
+
+const useStyles = makeStyles((theme) => ({
+  breadcrumbsContainer: {
+    ...theme.mixins.customize.breadcrumbs,
+    paddingBottom: 0,
+  },
+  content: {
+    padding: 24,
+    boxShadow: '0 10px 31px 0 rgba(0,0,0,0.05)',
+    [theme.breakpoints.down('sm')]: {
+      padding: 32,
+    },
+  },
+  orderItems: {
+    flexWrap: 'wrap',
+    paddingRight: 0,
+  },
+  items: {
+    flexBasis: '100%',
+    marginLeft: 56,
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+    '& .MuiTableCell-root': {
+      paddingLeft: 0,
+    },
+    '& .MuiTableCell-head': {
+      color: 'rgba(0, 0, 0, 0.54)',
+      fontWeight: 400,
+    },
+  },
+  largeImage: {
+    width: theme.spacing(6),
+    height: theme.spacing(8),
+  },
+  empty: {
+    ...theme.mixins.customize.centerFlex('column wrap'),
+    marginTop: 30,
+  },
+  cartTotalWrapper: {
+    marginTop: 22,
+    padding: 20,
+    fontSize: 16,
+    backgroundColor: '#F4F4F4',
+  },
+  cartTotal: {
+    fontSize: 18,
+    marginBottom: 8,
+    '&:nth-child(2)': {
+      color: theme.palette.secondary.main,
+    },
+  },
+  divider: {
+    margin: '8px 0',
+    width: 80,
+    height: 2,
+    backgroundColor: '#2a2a2a',
+  },
+  itemName: {
+    ...theme.mixins.customize.textClamp(2),
+  },
+}));
 
 const OrderScreen = ({ match, history }) => {
+  const classes = useStyles();
   const orderId = match.params.id;
 
   const [sdkReady, setSdkReady] = useState(false);
@@ -38,7 +129,7 @@ const OrderScreen = ({ match, history }) => {
     };
 
     order.itemsPrice = addDecimals(
-      order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      order.orderItems.reduce((acc, item) => acc + item.priceSale * item.qty, 0)
     );
   }
 
@@ -84,119 +175,189 @@ const OrderScreen = ({ match, history }) => {
   return loading ? (
     <Loader />
   ) : error ? (
-    <Message variant='danger'>{error}</Message>
+    <Message>{error}</Message>
   ) : (
-    <>
-      <Row>
-        <h1 style={{ color: '#0d6efd', padding: '28px', fontSize: 26 }}>
-          Order {order._id}
-        </h1>
-      </Row>
-      <Row>
-        <Col md={8}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h2>Shipping</h2>
-              <p>
-                <strong>Name: </strong> {order.user.name}
-              </p>
-              <p>
-                <strong>Email: </strong>{' '}
-                <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
-              </p>
-              <p>
-                <strong>Address:</strong>
-                {order.shippingAddress.address}, {order.shippingAddress.city}{' '}
-                {order.shippingAddress.postalCode},{' '}
-                {order.shippingAddress.country}
-              </p>
-              {order.isDelivered ? (
-                <Message variant='success'>
-                  Delivered on {order.deliveredAt}
-                </Message>
-              ) : (
-                <Message variant='danger'>Not Delivered</Message>
-              )}
-            </ListGroup.Item>
-
-            <ListGroup.Item>
-              <h2>Payment Method</h2>
-              <p>
-                <strong>Method: </strong>
-                {order.paymentMethod}
-              </p>
-              {order.isPaid ? (
-                <Message variant='success'>Paid on {order.paidAt}</Message>
-              ) : (
-                <Message variant='danger'>Not Paid</Message>
-              )}
-            </ListGroup.Item>
-
-            <ListGroup.Item>
-              <h2>Order Items</h2>
-              {order.orderItems.length === 0 ? (
-                <Message>Order is empty</Message>
-              ) : (
-                <ListGroup variant='flush'>
-                  {order.orderItems.map((item, index) => (
-                    <ListGroup.Item key={index} className='py-3'>
-                      <Row>
-                        <Col md={1}>
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            fluid
-                            rounded
-                          />
-                        </Col>
-                        <Col>
-                          <Link to={`/product/${item.product}`}>
-                            {item.name}
-                          </Link>
-                        </Col>
-                        <Col md={4}>
-                          {item.qty} x ${item.price} = ${item.qty * item.price}
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              )}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={4}>
-          <Card>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <h2>Order Summary</h2>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Items</Col>
-                  <Col>${order.itemsPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Shipping</Col>
-                  <Col>${order.shippingPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Tax</Col>
-                  <Col>${order.taxPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Total</Col>
-                  <Col>${order.totalPrice}</Col>
-                </Row>
-              </ListGroup.Item>
+    <Container maxWidth='xl' style={{ marginBottom: 48 }}>
+      <Meta title='Place Order | FashionShop' />
+      <Grid container className={classes.breadcrumbsContainer}>
+        <Grid item xs={12}>
+          <Breadcrumbs
+            separator={<NavigateNextIcon fontSize='small' />}
+            style={{ marginBottom: 24 }}
+          >
+            <Link color='inherit' component={RouterLink} to='/'>
+              Home
+            </Link>
+            <Link color='textPrimary' component={RouterLink} to='/order'>
+              Order Details
+            </Link>
+          </Breadcrumbs>
+        </Grid>
+      </Grid>
+      <Paper elevation={0} className={classes.content}>
+        <Grid container spacing={8}>
+          <Grid item xs={12} lg={8}>
+            <List>
+              <ListItem divider>
+                <ListItemText
+                  primary={`Order`}
+                  secondary={`id: ${order._id}`}
+                />
+              </ListItem>
+              <ListItem divider>
+                <ListItemIcon>
+                  <GrUser fontSize={22} />
+                </ListItemIcon>
+                <ListItemText
+                  primary='Receiver'
+                  secondary={`${order.user.name}, email: ${order.user.name}`}
+                />
+              </ListItem>
+              <ListItem divider style={{ flexWrap: 'wrap' }}>
+                <ListItemIcon>
+                  <GrLocation fontSize={22} />
+                </ListItemIcon>
+                <ListItemText
+                  primary='Shipping'
+                  secondary={Object.values(order.shippingAddress).join(', ')}
+                />
+                {order.isDelivered ? (
+                  <Message severity='success' mt={8}>
+                    Delivered on {new Date(order.deliveredAt).toUTCString()}
+                  </Message>
+                ) : (
+                  <Message mt={8}>Not Delivered</Message>
+                )}
+              </ListItem>
+              <ListItem divider style={{ flexWrap: 'wrap' }}>
+                <ListItemIcon>
+                  <GrCreditCard fontSize={22} />
+                </ListItemIcon>
+                <ListItemText
+                  primary='Payment Method'
+                  secondary={order.paymentMethod}
+                />
+                <ListItemAvatar>
+                  <img src={paypalImage} alt='' width='80px' height='30px' />
+                </ListItemAvatar>
+                {order.isPaid ? (
+                  <Message severity='success' mt={8}>
+                    Paid on {new Date(order.paidAt).toUTCString()}
+                  </Message>
+                ) : (
+                  <Message mt={8}>Not Paid</Message>
+                )}
+              </ListItem>
+              <ListItem className={classes.orderItems}>
+                <ListItemIcon>
+                  <GrProjects fontSize={22} />
+                </ListItemIcon>
+                <ListItemText primary='Order Items' />
+                {order.orderItems.length > 0 ? (
+                  <div className={classes.items}>
+                    <TableContainer component={Paper} elevation={0}>
+                      <Table>
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Products</TableCell>
+                            <Hidden smDown>
+                              <TableCell align='right'>Size</TableCell>
+                              <TableCell align='right'>Price</TableCell>
+                            </Hidden>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {order.orderItems.map((item) => (
+                            <TableRow key={item.name}>
+                              <TableCell component='th' scope='item'>
+                                <ListItem disableGutters>
+                                  <ListItemAvatar>
+                                    <Avatar
+                                      variant='square'
+                                      src={item.images && item.images[0]}
+                                      alt='product image'
+                                      className={classes.largeImage}
+                                    ></Avatar>
+                                  </ListItemAvatar>
+                                  <ListItemText
+                                    primary={item.name}
+                                    className={classes.itemName}
+                                    style={{ marginLeft: 16 }}
+                                  />
+                                </ListItem>
+                                <Hidden mdUp>
+                                  <Box
+                                    display='flex'
+                                    justifyContent='space-between'
+                                    alignItems='center'
+                                    mt={2}
+                                  >
+                                    <Box textAlign='center'>
+                                      Size: {item.sizeSelected.toUpperCase()}
+                                    </Box>
+                                    <Box textAlign='center'>
+                                      {`${item.qty} x ${item.priceSale} = ${
+                                        item.qty * item.priceSale
+                                      }`}
+                                    </Box>
+                                  </Box>
+                                </Hidden>
+                              </TableCell>
+                              <Hidden smDown>
+                                <TableCell align='right'>
+                                  {item.sizeSelected.toUpperCase()}
+                                </TableCell>
+                                <TableCell align='right'>
+                                  {`${item.qty} x $${item.priceSale} = $${
+                                    item.qty * item.priceSale
+                                  }`}
+                                </TableCell>
+                              </Hidden>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </div>
+                ) : (
+                  <div className={classes.empty}>
+                    <Typography variant='subtitle1' color='secondary'>
+                      Your cart is empty.{' '}
+                      <Link to='/' component={RouterLink} color='primary'>
+                        Shopping now!
+                      </Link>
+                    </Typography>
+                  </div>
+                )}
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} lg={4}>
+            <Paper elevation={0} className={classes.cartTotalWrapper}>
+              <Typography variant='h4' style={{ fontSize: 23 }}>
+                Order Summary
+              </Typography>
+              <Divider className={classes.divider} />
+              <List style={{ padding: '10px 20px 20px' }}>
+                <ListItem divider disableGutters>
+                  <ListItemText primary='Items:' />
+                  <Typography>${order.itemsPrice}</Typography>
+                </ListItem>
+                <ListItem divider disableGutters>
+                  <ListItemText primary='Shipping:' />
+                  <Typography>${order.shippingPrice}</Typography>
+                </ListItem>
+                <ListItem divider disableGutters>
+                  <ListItemText primary='Tax:' />
+                  <Typography>${order.taxPrice}</Typography>
+                </ListItem>
+                <ListItem disableGutters>
+                  <ListItemText primary='Total:' />
+                  <Typography color='secondary'>${order.totalPrice}</Typography>
+                </ListItem>
+              </List>
               {!order.isPaid && (
-                <ListGroup.Item>
+                <Box fullWidth>
                   {loadingPay && <Loader />}
                   {!sdkReady ? (
                     <Loader />
@@ -204,30 +365,32 @@ const OrderScreen = ({ match, history }) => {
                     <PayPalButton
                       amount={order.totalPrice}
                       onSuccess={successPaymentHandler}
+                      style={{ width: '100%' }}
                     />
                   )}
-                </ListGroup.Item>
+                </Box>
               )}
               {loadingDeliver && <Loader />}
               {userInfo &&
                 userInfo.isAdmin &&
                 order.isPaid &&
                 !order.isDelivered && (
-                  <ListGroup.Item>
+                  <Box>
                     <Button
-                      type='button'
-                      className='btn btn-block'
+                      variant='contained'
+                      color='secondary'
+                      fullWidth
                       onClick={deliverHandler}
                     >
                       Mark As Delivered
                     </Button>
-                  </ListGroup.Item>
+                  </Box>
                 )}
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
-    </>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
   );
 };
 
