88ea5b824d6b73f67555d26b295b4a4b4ad3406a
Phong Duy Bui <buiduyphong921@gmail.com>
2021-06-25 23:59:47 +0700

    Update HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,20 @@
 import React, { useEffect } from 'react';
-import queryString from 'query-string';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { listTopProducts } from '../actions/productActions';
 import Loader from '../components/Loader';
-import Message from '../components/Message';
 import Meta from '../components/Meta';
 import HomeCarousel from '../components/Home/HomeCarousel';
 import Container from '@material-ui/core/Container';
 import HomeBanner from '../components/Home/HomeBanner';
 import ProductCard from '../components/Product/ProductCard';
-import { CircularProgress, Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Link } from '@material-ui/core';
 import ProductTabs from '../components/Product/ProductTabs';
 import HomeService from '../components/Home/HomeService';
-import { makeStyles } from '@material-ui/core/styles';
 import Alert from '@material-ui/lab/Alert';
 
-const useStyles = makeStyles((theme) => ({
-  loading: {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-    marginTop: 40,
-    marginBottom: 40,
-  },
-}));
-
 const HomeScreen = ({ location }) => {
-  const classes = useStyles();
   const dispatch = useDispatch();
-  const { keyword, pageNumber = 1 } = queryString.parse(location.search);
   const productTopRated = useSelector((state) => state.productTopRated);
 
   const {
@@ -39,21 +24,13 @@ const HomeScreen = ({ location }) => {
   } = productTopRated;
 
   useEffect(() => {
-    dispatch(listTopProducts());
-  }, [dispatch, keyword, pageNumber]);
+    dispatch(listTopProducts(1, 8));
+  }, [dispatch]);
 
   return (
     <>
       <Meta />
-      {!keyword ? (
-        <>
-          <HomeCarousel />
-        </>
-      ) : (
-        <Link to='/' className='btn btn-light'>
-          Go Back
-        </Link>
-      )}
+      <HomeCarousel />
       <Container maxWidth='xl'>
         <Typography
           variant='h4'
@@ -64,19 +41,30 @@ const HomeScreen = ({ location }) => {
           Top Products
         </Typography>
         {loadingProductTop ? (
-          <div className={classes.loading}>
-            <CircularProgress color='secondary' />
-          </div>
+          <Loader />
         ) : errorProductTop ? (
           <Alert severity='error'>{errorProductTop}</Alert>
         ) : (
           <>
-            <Grid container spacing={3} className={classes.topProducts}>
+            <Grid container spacing={3}>
               {productTop.map((product) => (
                 <Grid item xs={12} sm={6} md={4} lg={3} key={product._id}>
                   <ProductCard {...product} />
                 </Grid>
               ))}
+              <Grid
+                item
+                xs={12}
+                style={{
+                  display: 'flex',
+                  flexBasis: '100%',
+                  justifyContent: 'flex-end',
+                }}
+              >
+                <Link component={RouterLink} to='/shop?sortBy=rating'>
+                  See More...
+                </Link>
+              </Grid>
             </Grid>
             <HomeBanner />
             <ProductTabs />
