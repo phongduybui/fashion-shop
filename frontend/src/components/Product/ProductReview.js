669ecd59166e773e66d9702b5bbd18cb705bc940
Phong Duy Bui <buiduyphong921@gmail.com>
2021-06-26 00:04:17 +0700

    Add post review function

--- frontend/src/components/Product/ProductReview.js
+++ frontend/src/components/Product/ProductReview.js
@@ -5,79 +5,148 @@ import {
   Grid,
   Paper,
   Typography,
+  TextField,
+  Link,
+  Button,
 } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link as RouterLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { PRODUCT_CREATE_REVIEW_RESET } from '../../constants/productConstants.js';
+import { createProductReview } from '../../actions/productActions.js';
+import Message from '../Message';
+import Loader from '../Loader';
 
-const ProductReview = () => {
+const useStyles = makeStyles((theme) => ({
+  form: {
+    ...theme.mixins.customize.flexMixin('center', 'flex-start', 'column'),
+    '& > *': {
+      marginBottom: 16,
+    },
+  },
+}));
+
+const ProductReview = ({ reviews, productId }) => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const productReviewCreate = useSelector((state) => state.productReviewCreate);
+  const {
+    success: successProductReview,
+    loading: loadingProductReview,
+    error: errorProductReview,
+  } = productReviewCreate;
+
+  const handleSubmitReview = (e) => {
+    e.preventDefault();
+    dispatch(
+      createProductReview(productId, {
+        rating,
+        comment,
+      })
+    );
+  };
+
+  useEffect(() => {
+    if (successProductReview) {
+      setRating(0);
+      setComment('');
+    }
+    if (!productId) {
+      dispatch({ type: PRODUCT_CREATE_REVIEW_RESET });
+    }
+  }, [dispatch, successProductReview, productId]);
+
   return (
     <>
       <Box my={3}>
         <Typography variant='h5'>Reviews</Typography>
       </Box>
-      <Paper style={{ padding: '32px 20px', margin: '24px 0' }} elevation={0}>
-        <Grid container wrap='nowrap' spacing={2}>
-          <Grid item>
-            <Avatar
-              alt='Remy Sharp'
-              src='https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
-            />
-          </Grid>
-          <Grid item xs zeroMinWidth>
-            <Box display='flex' alignItems='center'>
-              <h4
-                style={{
-                  margin: '0 8px 0 0',
-                  textAlign: 'left',
-                  display: 'inline-block',
-                }}
-              >
-                Michel Michel
-              </h4>
-              <p style={{ margin: 0, textAlign: 'left', color: 'gray' }}>
-                posted 1 minute ago
-              </p>
-            </Box>
-            <Rating name='read-only' value={5} readOnly size='small' />
-            <p style={{ textAlign: 'left', marginTop: 5 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              luctus ut est sed faucibus. Duis bibendum ac ex vehicula laoreet.
-              Suspendisse congue vulputate lobortis. Pellentesque at interdum
-              tortor. Quisque arcu quam, malesuada vel mauris et, posuere
-              sagittis ipsum.{' '}
-            </p>
-          </Grid>
-        </Grid>
-        <Divider variant='fullWidth' style={{ margin: '30px 0' }} />
-        <Grid container wrap='nowrap' spacing={2}>
-          <Grid item>
-            <Avatar
-              alt='Remy Sharp'
-              src='https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
-            />
-          </Grid>
-          <Grid item xs zeroMinWidth>
-            <Box display='flex' alignItems='center'>
-              <h4
-                style={{
-                  margin: '0 8px 0 0',
-                  textAlign: 'left',
-                  display: 'inline-block',
-                }}
-              >
-                Michel Michel
-              </h4>
-              <p style={{ margin: 0, textAlign: 'left', color: 'gray' }}>
-                posted 1 minute ago
-              </p>
-            </Box>
-            <Rating name='read-only' value={5} readOnly size='small' />
-            <p style={{ textAlign: 'left', marginTop: 5 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              luctus ut est sed faucibus. Duis bibendum ac ex vehicula laoreet.
-              Suspendisse congue vulputate lobortis. Pellentesque at interdum
-              tortor.{' '}
-            </p>
+      <Paper style={{ padding: 20, margin: '24px 0' }} elevation={0}>
+        {reviews.map((review) => (
+          <>
+            <Grid container wrap='nowrap' spacing={2}>
+              <Grid item>
+                <Avatar
+                  alt='avatar'
+                  src={`https://ui-avatars.com/api/?background=random&color=fff&name=${review.name}`}
+                />
+              </Grid>
+              <Grid item xs zeroMinWidth>
+                <Box display='flex' alignItems='center'>
+                  <h4
+                    style={{
+                      margin: '0 8px 0 0',
+                      textAlign: 'left',
+                      display: 'inline-block',
+                    }}
+                  >
+                    {review.name}
+                  </h4>
+                  <p style={{ margin: 0, textAlign: 'left', color: 'gray' }}>
+                    {review.createdAt.substring(0, 10)}
+                  </p>
+                </Box>
+                <Rating
+                  name='read-only'
+                  value={review.rating}
+                  precision={0.5}
+                  readOnly
+                  size='small'
+                />
+                <p style={{ textAlign: 'left', marginTop: 5 }}>
+                  {review.comment}
+                </p>
+              </Grid>
+            </Grid>
+            <Divider variant='fullWidth' style={{ margin: '30px 0' }} />
+          </>
+        ))}
+        <Grid container>
+          <Grid item xs={12}>
+            {loadingProductReview && <Loader />}
+            {errorProductReview && <Message>{errorProductReview}</Message>}
+            {userInfo ? (
+              <form onSubmit={handleSubmitReview} className={classes.form}>
+                <Typography variant='h5'>Write a review</Typography>
+                <Rating
+                  value={rating}
+                  precision={0.5}
+                  onChange={(event, newValue) => {
+                    setRating(newValue);
+                  }}
+                />
+                <TextField
+                  variant='outlined'
+                  label='Comment'
+                  multiline
+                  fullWidth
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                ></TextField>
+                <Button variant='contained' color='secondary' type='submit'>
+                  Submit
+                </Button>
+              </form>
+            ) : (
+              <Message severity='info'>
+                Please{' '}
+                <Link
+                  component={RouterLink}
+                  to={`/login?redirect=/product/${productId}`}
+                >
+                  login
+                </Link>{' '}
+                to write a review
+              </Message>
+            )}
           </Grid>
         </Grid>
       </Paper>
